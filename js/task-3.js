fdd420a306712bbc784503c51e98b3296ee47c44
valeriya2310 <vp0732022011@gmail.com>
2025-03-25 21:02:24 +0200

    update index.html file

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = (users) =>
-  users.slice().sort((a, b) => b.friends.length - a.friends.length);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 
 
